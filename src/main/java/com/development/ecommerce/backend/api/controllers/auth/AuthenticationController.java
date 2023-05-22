887ec60a183517f519bc64d44d7500d1c24f4851
FadyGamilM <1700961@eng.asu.edu.eg>
2023-05-22 13:48:59 +0300

    FEATURE | implement the Auth-Register endpoint

--- src/main/java/com/development/ecommerce/backend/api/controllers/auth/AuthenticationController.java
+++ src/main/java/com/development/ecommerce/backend/api/controllers/auth/AuthenticationController.java
@@ -1,17 +1,26 @@
 package com.development.ecommerce.backend.api.controllers.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.development.ecommerce.backend.api.models.RegisterationDto;
+import com.development.ecommerce.backend.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
+    @Autowired
+    private AuthService _service;
+
+    AuthenticationController(AuthService service){
+        _service = service;
+    }
+
     @PostMapping("/register")
-    public void Register(@RequestBody RegisterationDto dto){
-        
+    public Long Register(@RequestBody RegisterationDto dto){
+        return _service.RegisterCustomer(dto).getId();
     }
 }
